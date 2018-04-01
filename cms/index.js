e2e234da47a6fe160197173af1d0d54de5814024
root <root@nodejs.hxrsmurf.info>
2018-04-01 15:19:01 -0400

    updated index again

--- cms/index.js
+++ cms/index.js
@@ -5,45 +5,17 @@
 	},
 	announcements: [
 		{
-			announcement_body: 'None'
+			announcement_body: '01Apr2018: Moved to EnduroJS'
+		},
+		{
+			announcement_body: '04July2015: New CSS Template'
 		}
 	],
 	post_body: [
 		{
-			post_title: 'Contact',
-			post_body: ''
+			post_title: 'Welcome',
+			post_body: 'Hi, my name is Kevin "hXrsmurf" Murphy and you can find my contact information here if you have any questions. I\'m slowly working on this website to improve it and add content to it. If you would like to view the previous versions of my announcements, click here. Thanks for visiting!'
 		}
 	],
-	images: [
-		{
-			image_url: 'https://facebook.com/hxrsmurf',
-			$image_type: 'image',
-			image: 'https://pbs.twimg.com/profile_images/3513354941/24aaffa670e634a7da9a087bfa83abe6_400x400.png'
-		},
-		{
-			image_url: 'https://twitter.com/hxrsmurf',
-			$image_type: 'image',
-			image: 'https://pbs.twimg.com/profile_images/875087697177567232/Qfy0kRIP_400x400.jpg'
-		},
-		{
-			image_url: 'https://steamcommunity.com/id/hxrsmurf/',
-			$image_type: 'image',
-			image: 'https://pbs.twimg.com/profile_images/887778636102721536/Nxgl7xz4_400x400.jpg'
-		},
-		{
-			image_url: 'https://www.linkedin.com/pub/kevin-murphy/5a/57b/213/',
-			$image_type: 'image',
-			image: 'https://pbs.twimg.com/profile_images/614583061448036352/CBpFkPaz_400x400.png'
-		},
-		{
-			image_url: 'https://youtube.com/pngassassin1993',
-			$image_type: 'image',
-			image: 'https://pbs.twimg.com/profile_images/972139138710417411/d3GiC2It_400x400.jpg'
-		},
-		{
-			image_url: 'https://twitch.tv/hxrsmurf',
-			$image_type: 'image',
-			image: 'https://pbs.twimg.com/profile_images/979092312553750528/2ejlMVyG_400x400.jpg'
-		}
-	]
+	images: []
 })
